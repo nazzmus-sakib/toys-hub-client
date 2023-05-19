1ac5aa105dc3cf0c7c79ae2ec6f9504ac2c27835
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-05-19 12:04:18 +0600

    initial set-up done

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+  },
+]);
+export default router;
